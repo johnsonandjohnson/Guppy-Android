d629ea2d38859c92a4802a8f9970251c8799e29f
Niezrecki, Kamil [GPSIE] <KNiezrec@ITS.JNJ.com>
2019-10-16 12:18:30 +0100

    Add TextView label to main activity to open Guppy directly instead of shaking.

--- guppy/src/main/java/com/jnj/guppy/GuppyActivity.kt
+++ guppy/src/main/java/com/jnj/guppy/GuppyActivity.kt
@@ -44,10 +44,11 @@ abstract class GuppyActivity : AppCompatActivity(), ShakeAction {
     var dialogFragment: GuppyDialogFragment? = null
 
     override fun setSensors() {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as? SensorManager?
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        shakeDetector = ShakeDetector()
-        shakeDetector?.setOnShakeListener(shakeListener)
+        shakeDetector = ShakeDetector().apply {
+            setOnShakeListener(shakeListener)
+        }
     }
 
     override fun onShakeReceived(dialogFragment: GuppyDialogFragment?) {
